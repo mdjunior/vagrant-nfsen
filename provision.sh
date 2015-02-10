ec8771be468c37b597c10b08eec90897c2b16d96
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2015-02-10 12:38:30 -0200

    [FIX] Clean white space

--- provision.sh
+++ provision.sh
@@ -1,41 +1,61 @@
 #!/usr/bin/env bash
 
-perl -pi -e "s/br/us/" /etc/apt/sources.list
 
+#  Fix error message in system
+sudo locale-gen UTF-8
 
-apt-get update
-apt-get upgrade -y
+#  Updating repos
+sudo apt-get update -y
 
+#  Install deps for nfdump
+apt-get install -y libapache2-mod-php5 librrds-perl libmailtools-perl libsocket6-perl rrdtool whois
+
+#  Install nfdump
 apt-get install -y nfdump
 
-apt-get install -y libapache2-mod-php5   librrds-perl libmailtools-perl libsocket6-perl rrdtool whois
-
+#  Verify if mod_rewrite is set
 if [ ! -e /etc/apache2/mods-enabled/rewrite.load ] ; then
     a2enmod rewrite
     service apache2 restart
 fi
 
+#  Checking files, and downloading if they do not exist
+if [ ! -e nfsen.tar.gz ] ; then
+    wget -q http://sourceforge.net/projects/nfsen/files/stable/nfsen-1.3.7/nfsen-1.3.7.tar.gz/download -k -O nfsen.tar.gz
+    wget -q http://sourceforge.net/projects/nfsen/files/stable/nfsen-1.3.7/nfsen-1.3.7.tar.gz.sig/download -k -O nfsen.tar.gz.sig
 
-if [ ! -e nfsen-1.3.6p1.tar.gz ] ; then
-    wget http://iweb.dl.sourceforge.net/project/nfsen/stable/nfsen-1.3.6p1/nfsen-1.3.6p1.tar.gz
-    tar xvzf nfsen-1.3.6p1.tar.gz
+    gpg --keyserver pgp.mit.edu --recv-key EDE80DF6
+    gpg --verify nfsen.tar.gz.sig
+    if [ "$?" != "0" ] ; then
+        echo "=> Signature not match!"
+        exit
+    fi
 fi
 
+#  Unpack files
+tar xvzf nfsen.tar.gz
+
+#  Preparing for the installation of nfsen
 mkdir -p /data/nfsen
 
+#  If nfsen is not installed
 if [ ! -e /var/www/nfsen ] ; then 
-    cd nfsen-1.3.6p1/
+    cd nfsen-*/
     yes ''| ./install.pl /vagrant_data/nfsen.conf
     cd /var/www/nfsen
     ln -s nfsen.php index.php
 fi
 
+#  Change the apache configuration for nfsen
+sudo sed -i "s/DocumentRoot.*/DocumentRoot \/var\/www/" /etc/apache2/sites-enabled/000-default.conf
+
+#  Restarting nfsen
 /data/nfsen/bin/nfsen stop
 /data/nfsen/bin/nfsen start
 
-#setup fprobe
-
-echo "fprobe fprobe/collector string localhost:9995" | debconf-set-selections
+#  Setup fprobe
+echo "fprobe fprobe/collector string localhost:9996" | debconf-set-selections
 echo "fprobe fprobe/interface string eth0" | debconf-set-selections
 
+#  Install fprobe
 DEBIAN_FRONTEND=noninteractive apt-get -y install fprobe
